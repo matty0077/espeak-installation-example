fe61367967e3d84127f40cb59e484503c1c4edb5
matty0077 <Moquendo993@gmail.com>
2018-07-30 20:05:43 +0000

    Add files via upload

--- espeak.py
+++ espeak.py
@@ -5,7 +5,9 @@ port='hey how have you been?'
 
 def say(something):
     try:
-        os.system('sudo espeak  "{0}"'.format(something))
+        import subprocess#customizable voice quality voice,talk speed, pitch,volume
+        voice1=subprocess.Popen(["espeak", "-v", 'en-us1',"-s",'175',"-p",'75',"-a",'80', something])
+        #os.system('sudo espeak  "{0}"'.format(something))
     except Exception as e:
         print(e.message)
 
